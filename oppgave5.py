23251e9b9aa4b759bd89b7cc9fec0280824467db
henrikkriki <henriknormann@hotmail.no>
2025-09-04 10:55:00 +0200

    initial commit

---
+++ oppgave5.py
@@ -0,0 +1,10 @@
+ord = input("skriv inn setningen")
+lengde=len(ord)
+
+print ("ordet", '"'+ ord +'"', "inneholder", lengde, "bokstaver")
+
+navn=input ("hva_heter_du?")
+navn=print("hei,"+navn+" velkommen til deg!")
+
+navn =input(ord)
+print ("ordet",'"'+ navn +'"', "inneholder", lengde, "bokstaver")
